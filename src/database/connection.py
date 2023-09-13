a70dcfd83f07a8a14bf5ffaffe3e925485c50298
Luceta <57941164+Luceta@users.noreply.github.com>
2023-09-13 15:52:08 +0900

    [FEAT]: database connection

---
+++ src/database/connection.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from core.config import settings
+
+
+DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionFactory = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+
+def get_db():
+    session = SessionFactory()
+    if session is None:
+        raise Exception("session is not connected")
+    try:
+        yield session
+    finally:
+        session.close()
